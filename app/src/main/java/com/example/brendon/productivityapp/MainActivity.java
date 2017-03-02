73365a3df33bbca6945d292822d92279b9f69b9a
tyler-willis <wil14092@byui.edu>
2017-03-01 21:01:56 -0700

    The app now checks if the user has enables the correct permissions. If not, it opens the system settings.

--- app/src/main/java/com/example/brendon/productivityapp/MainActivity.java
+++ app/src/main/java/com/example/brendon/productivityapp/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.brendon.productivityapp;
 
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String PREFS_NAME = "savedSettings";
+    private static final int MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS = 100;
     //public static final String USAGE_STATS_SERVICE = "usagestats";
     //SharedPreferences settingsPref = getSharedPreferences(PREFS_NAME, 0);
 
@@ -14,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check if the user has enabled Usage Stats. If not, request it
+        if (!hasPermission())
+            requestPermission();
 
         //Loading settings, commented out because doesn't work yet
         /*Gson gson = new Gson();
@@ -42,4 +52,22 @@ public class MainActivity extends AppCompatActivity {
         settingEditor.commit();*/
     }
 
+    // Checks if the user has granted permission to the app
+    private boolean hasPermission() {
+        AppOpsManager appOps = (AppOpsManager)
+                getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                android.os.Process.myUid(), getPackageName());
+        return mode == AppOpsManager.MODE_ALLOWED;
+    }
+
+    private void requestPermission() {
+        Toast.makeText(getApplicationContext(),
+                "You must allow the app permission to access Usage Stats./nOpening System Settings.",
+                Toast.LENGTH_SHORT).show();
+
+        startActivityForResult(
+                new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS),
+                MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS);
+    }
 }
