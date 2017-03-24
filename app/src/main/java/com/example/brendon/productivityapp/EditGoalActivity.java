e9a4519fa3044b591826a13dc25d842d7ec2d202
scott56gps <nic15007@byui.edu>
2017-03-24 13:55:03 -0600

    Updated editGoalActivity. Created View Skeleton for this Activity.

--- app/src/main/java/com/example/brendon/productivityapp/EditGoalActivity.java
+++ app/src/main/java/com/example/brendon/productivityapp/EditGoalActivity.java
@@ -2,8 +2,13 @@ package com.example.brendon.productivityapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditGoalActivity extends AppCompatActivity {
+    TextView editGoalHeader;
+    EditText hoursEditor;
+    EditText minutesEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
