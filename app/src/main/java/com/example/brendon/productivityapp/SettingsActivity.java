b31e4da2bb2fa3c3fee0bcbc766654190c832252
scott56gps <nic15007@byui.edu>
2017-03-27 14:13:49 -0600

    Finsihed Linking Activities in FirstStartup direction.

--- app/src/main/java/com/example/brendon/productivityapp/SettingsActivity.java
+++ app/src/main/java/com/example/brendon/productivityapp/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.example.brendon.productivityapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -23,4 +25,11 @@ public class SettingsActivity extends AppCompatActivity {
         ListView list=(ListView)findViewById(R.id.list);
         list.setAdapter(adapter);
     }
+
+    public void startEditPlan(View view) {
+        Intent intent = new Intent(this, EditPlan.class);
+
+        startActivity(intent);
+    }
+
 }
